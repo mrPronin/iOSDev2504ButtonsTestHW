2660649ae0e3b83f2b8d79cfccadaa3fa290b2fd
Alexander Pronin <pronin.alx@gmail.com>
2014-04-19 18:34:50 +0300

    Initial Commit

---
+++ 2501ButtonsTestHW/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  2501ButtonsTestHW
+//
+//  Created by Pronin Alexander on 19.04.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
---
+++ 2501ButtonsTestHW/RITViewController.m
@@ -0,0 +1,29 @@
+//
+//  RITViewController.m
+//  2501ButtonsTestHW
+//
+//  Created by Pronin Alexander on 19.04.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import "RITViewController.h"
+
+@interface RITViewController ()
+
+@end
+
+@implementation RITViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
